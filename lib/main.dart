aa33c5d0873226fd23ad66ef89da0aa1e5e52d81
Nahid1828 <isam15-5334@diu.edu.bd>
2025-02-18 11:08:59 +0600

    Task_2

--- lib/main.dart
+++ lib/main.dart
@@ -10,50 +10,48 @@ class Test extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-         title: "Basic-UI - Flutter",
-         home: Scaffold(
-           appBar: AppBar(
-             title: Text("Flutter App"),
-             centerTitle: true,
-             elevation: 10.5,
-           ),
-           body: Column( 
-             mainAxisAlignment: MainAxisAlignment.center, 
-             crossAxisAlignment: CrossAxisAlignment.center, 
-             children: [
-               Row(
-                 mainAxisAlignment: MainAxisAlignment.center, 
-                 children: [
-                   Text(
-                     "Hi ",
-                     style: TextStyle(
-                       color: Colors.red,
-                       fontSize: 30,
-                     ),
-                   ),
-                   Text(
-                     "Flutter",
-                     style: TextStyle(
-                       color: Colors.blue,
-                       fontSize: 30,
-                     ),
-                   )
-                 ],
-               ),
-               SizedBox(height: 20), 
-               Row(
-                 mainAxisAlignment: MainAxisAlignment.center,
-                 children: [
-                   Icon(Icons.star, color: Colors.yellow), 
-                   Icon(Icons.star, color: Colors.yellow), 
-                   Icon(Icons.star, color: Colors.yellow),
-                   Icon(Icons.star, color: Colors.yellow), 
-                   Icon(Icons.star_border, color: Colors.yellow), 
-                 ],
-               ),
-             ],
-           ),
-         ),
+      title: "Basic-UI - Flutter",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Flutter App"),
+          centerTitle: true,
+          elevation: 10.5,
+        ),
+        body: Container(
+          width: double.infinity,
+          height: double.infinity,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly, 
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.ac_unit,
+                size: 30,
+                color: const Color.fromARGB(255, 0, 217, 255),
+              ),
+              Icon(
+                Icons.safety_check,
+                size: 30,
+                color: const Color.fromARGB(255, 9, 217, 0),
+              ),
+              Icon(
+                Icons.mail,
+                size: 30,
+                color: const Color.fromARGB(255, 221, 0, 255),
+              ),
+              Row(
+                children: [
+                  Icon(Icons.star, color: Colors.yellow),
+                  Icon(Icons.star, color: Colors.yellow),
+                  Icon(Icons.star, color: Colors.yellow),
+                  Icon(Icons.star, color: Colors.yellow),
+                  Icon(Icons.star_border, color: Colors.yellow),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
