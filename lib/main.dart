d4442d718328bdeebe32ea0a9ad785e939097b1f
Nahid1828 <isam15-5334@diu.edu.bd>
2025-02-18 11:31:41 +0600

    Task_3

--- lib/main.dart
+++ lib/main.dart
@@ -17,38 +17,21 @@ class Test extends StatelessWidget {
           centerTitle: true,
           elevation: 10.5,
         ),
-        body: Container(
-          width: double.infinity,
-          height: double.infinity,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly, 
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Icon(
-                Icons.ac_unit,
-                size: 30,
-                color: const Color.fromARGB(255, 0, 217, 255),
+        body: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Container(
+            height: 600,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: List.generate(
+                100,
+                (index) => Icon(
+                  Iconsax.activity,
+                  size: index.toDouble(),
+                  color: const Color.fromARGB(255, 21, 0, 255)),
               ),
-              Icon(
-                Icons.safety_check,
-                size: 30,
-                color: const Color.fromARGB(255, 9, 217, 0),
-              ),
-              Icon(
-                Icons.mail,
-                size: 30,
-                color: const Color.fromARGB(255, 221, 0, 255),
-              ),
-              Row(
-                children: [
-                  Icon(Icons.star, color: Colors.yellow),
-                  Icon(Icons.star, color: Colors.yellow),
-                  Icon(Icons.star, color: Colors.yellow),
-                  Icon(Icons.star, color: Colors.yellow),
-                  Icon(Icons.star_border, color: Colors.yellow),
-                ],
-              ),
-            ],
+            ),
           ),
         ),
       ),
