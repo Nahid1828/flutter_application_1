94280ce2047bb50b15267b8c00bda0a00816bdca
Nahid1828 <isam15-5334@diu.edu.bd>
2025-02-18 10:21:01 +0600

    Task_1

--- lib/main.dart
+++ lib/main.dart
@@ -17,24 +17,42 @@ class Test extends StatelessWidget {
              centerTitle: true,
              elevation: 10.5,
            ),
-           body: Row(
-            children: [
-              Text(
-                "Hi ",
-                style: TextStyle(
-                  color: Colors.red,
-                  fontSize: 30,
-                ),
-              ),
-              Text(
-                "Flutter",
-                style: TextStyle(
-                  color: Colors.blue,
-                  fontSize: 30,
-                ),
-              )
-            ],
-            ),
+           body: Column( 
+             mainAxisAlignment: MainAxisAlignment.center, 
+             crossAxisAlignment: CrossAxisAlignment.center, 
+             children: [
+               Row(
+                 mainAxisAlignment: MainAxisAlignment.center, 
+                 children: [
+                   Text(
+                     "Hi ",
+                     style: TextStyle(
+                       color: Colors.red,
+                       fontSize: 30,
+                     ),
+                   ),
+                   Text(
+                     "Flutter",
+                     style: TextStyle(
+                       color: Colors.blue,
+                       fontSize: 30,
+                     ),
+                   )
+                 ],
+               ),
+               SizedBox(height: 20), 
+               Row(
+                 mainAxisAlignment: MainAxisAlignment.center,
+                 children: [
+                   Icon(Icons.star, color: Colors.yellow), 
+                   Icon(Icons.star, color: Colors.yellow), 
+                   Icon(Icons.star, color: Colors.yellow),
+                   Icon(Icons.star, color: Colors.yellow), 
+                   Icon(Icons.star_border, color: Colors.yellow), 
+                 ],
+               ),
+             ],
+           ),
          ),
     );
   }
