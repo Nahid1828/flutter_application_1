4e8a257d71572b7aa4201506e78e6b2639845b84
Nahid1828 <isam15-5334@diu.edu.bd>
2025-02-18 12:59:52 +0600

    Task_4

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,96 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const Test());
+  runApp(MyApp());
 }
 
-class Test extends StatelessWidget {
-  const Test({super.key});
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Basic-UI - Flutter",
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Flutter App"),
-          centerTitle: true,
-          elevation: 10.5,
+          title: Text('Nahid Flutter App'),
+          backgroundColor: Colors.amber,
         ),
-        body: SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Container(
-            height: 600,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: List.generate(
-                100,
-                (index) => Icon(
-                  Iconsax.activity,
-                  size: index.toDouble(),
-                  color: const Color.fromARGB(255, 21, 0, 255)),
+        body: Center(
+          child: ProductCard(),
+        ),
+      ),
+    );
+  }
+}
+
+class ProductCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 300,
+      padding: EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(16),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.3),
+            blurRadius: 10,
+            spreadRadius: 2,
+          ),
+        ],
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+            decoration: BoxDecoration(
+              color: Colors.orange,
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Text(
+              '80% OFF',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
               ),
             ),
           ),
-        ),
+          SizedBox(height: 10),
+          
+          Text(
+            'Nike Air Shoes',
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 5),
+          // Prices
+          Row(
+            children: [
+              Text(
+                '\$450',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+              SizedBox(width: 10),
+              Text(
+                '\$900',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.grey,
+                  decoration: TextDecoration.lineThrough,
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
